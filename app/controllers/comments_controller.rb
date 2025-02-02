0bbd136fc6d5ee02b6ee5fbab8337f3d952076cd
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2025-02-02 16:04:44 +0300

    feat(comment): update comment count without reload

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,8 @@ class CommentsController < ApplicationController
       if @comment.save
         format.turbo_stream {
           render turbo_stream: [ turbo_stream.append("comments_list", partial: "comments/comment", locals: { comment: @comment }),
-           turbo_stream.replace("comment_form", partial: "comments/form", locals: { comment: Comment.new })
+           turbo_stream.replace("comment_form", partial: "comments/form", locals: { comment: Comment.new }),
+          turbo_stream.replace("comment_count", partial: "posts/comment_count", locals: { post: @post })
           ]
         }
         format.html { redirect_to @post, notice: "Comment was successfully created." }
@@ -46,6 +47,11 @@ class CommentsController < ApplicationController
     @comment.destroy!
 
     respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: [  turbo_stream.remove(@comment),
+          turbo_stream.replace("comment_count", partial: "posts/comment_count", locals: { post: @post })
+        ]
+      }
       format.html { redirect_to @post, status: :see_other, notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
     end
