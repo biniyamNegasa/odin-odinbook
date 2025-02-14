59ba98f45c7eb311b2a79a31d6b81d6185ce8ad2
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2025-02-14 18:55:44 +0300

    feat(post): show like updates fast

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,12 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @post.likes.create(user: current_user)
-    redirect_back fallback_location: root_path
+    render partial: "likes/like", locals: { post: @post }
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @post.likes.find_by(user: current_user).destroy
-    redirect_back fallback_location: root_path
+    render partial: "likes/like", locals: { post: @post }
   end
 end
