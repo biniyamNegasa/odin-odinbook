602c66ec975c604087e9ac893cddd2672563ad42
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2025-02-15 11:21:24 +0300

    feat: add host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,6 +82,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: "odinbook.koyeb.app", protocol: "https" }
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
